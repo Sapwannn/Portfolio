27ee4e3578e07e56627359cb4357c5fdb9cd1a63
Sapwannn <felixasshafwan1@gmail.com>
2022-11-16 11:39:27 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,56 @@
+// for (var a = 0; a <= 7; a += 3) {
+//     console.log("apaakek" + a);
+// }
+
+// for (var b = 1; b <= 4; b++) {
+//     for (var c = 1; c <= 2; c++) {
+//         console.log("var a : " + b + "var keuda : " + c);
+//     }
+// }
+
+// while
+
+// var i =99;
+// while (i >=1) {
+//     console.log("i while ke  " + i);
+//     i-= 2;
+// }
+
+// var d = 1;
+// while (d <= 10) {
+//     console.log("apakek ke" + d);
+//     d++;
+// }
+
+
+// var z = 100;
+// while (z >= 1) {
+//     console.log("pacar ke : " + z);
+//     z--;
+// }
+
+for (var f = 0; f <= 30; f+= 3) {
+    console.log("haii" + f);
+}
+
+
+// do while //
+var x = 1;
+do {
+    console.log(`hello js ${x}`);
+    x++;
+} while (x <= 10)
+
+
+for (var k =1; k<= 100; k++){
+    console.log(k + " di kali " + k + k);
+}
+
+var siswa = ["farrel", "yahya", "izzan", "faiq",];
+for (var v = 0; v <= 4; v++) {
+    console.log(siswa[v]);
+}
+var siswa = ["farrel", "yahya", "izzan", "faiq",];
+for (var g = 0; g < siswa.length; g++) {
+    console.log(siswa[g]);
+}
